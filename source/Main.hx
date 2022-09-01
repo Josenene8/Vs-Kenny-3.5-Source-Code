4b49fd565b174b7bc056d64c7708efdd7430bed0
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-09-01 20:10:16 -0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -110,10 +110,7 @@ class Main extends Sprite
 			gameHeight = Math.ceil(stageHeight / zoom);
 		}
 
-		#if !debug
-		initialState = TitleState;
-		#end
-
+		
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
 
 		#if !mobile
@@ -122,8 +119,7 @@ class Main extends Sprite
 		if(fpsVar != null) {
 			fpsVar.visible = ClientPrefs.showFPS;
 		}
-		#end
-
+		
 		#if html5
 		FlxG.autoPause = false;
 		FlxG.mouse.visible = false;
