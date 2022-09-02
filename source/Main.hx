ea30b8c8156e736246bd2e5256e8159735e03078
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-09-02 10:14:00 -0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -111,8 +111,7 @@ class Main extends Sprite
 		}
 
 		
-		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
-
+		
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsVar);
 		if(fpsVar != null) {
