33ea2ba66de5655f24cd7fee098a6779a16cd546
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-09-02 11:19:03 -0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -110,7 +110,7 @@ class Main extends Sprite
 			gameHeight = Math.ceil(stageHeight / zoom);
 		}
 
-		
+		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
 		
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsVar);
